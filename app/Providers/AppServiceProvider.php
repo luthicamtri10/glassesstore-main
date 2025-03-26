bc862e15a495b8b2cc8281c6c31276c0304f7caa
luthicamtri10 <luthicamtri29102004@gmail.com>
2025-03-26 22:07:54 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Bus\ChucNang_BUS;
+use App\Bus\CPVC_BUS;
+use App\Dao\ChucNang_DAO;
+use App\Dao\CPVC_DAO;
+use App\Models\ChucNang;
+use App\Models\CPVC;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ChucNang_DAO::class, function ($app) {
+            return new ChucNang_DAO();
+        });
+        $this->app->singleton(ChucNang_BUS::class, function ($app) {
+            return new ChucNang_BUS($app->make(ChucNang_DAO::class));
+        });
+        $this->app->bind(CPVC_DAO::class, function ($app) {
+            return new CPVC_DAO();
+        });
+        $this->app->singleton(CPVC_BUS::class, function ($app) {
+            return new CPVC_BUS($app->make(CPVC_DAO::class));
+        });
+       
+        
     }
 
     /**
